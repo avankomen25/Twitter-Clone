c3b787a1c236895e82517b71f6e67a54991fdec5
Andrew Van Komen <avankomen25@cmc.edu>
2026-04-27 11:57:18 -0700

    make templating generic

--- main.py
+++ main.py
@@ -17,5 +17,12 @@ async def index(request: Request):
         name='index.html',
     )
 
+@app.get('/login', response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name='login.html',
+    )
+
 if __name__ == '__main__':
     uvicorn.run("main:app", host='127.0.0.1', port=8080, reload=True)
