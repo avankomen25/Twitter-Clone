58a05ce96b43a44c2dcf5c39e53b1ddf2ce4fac9
Andrew Van Komen <avankomen25@cmc.edu>
2026-04-27 11:40:40 -0700

    add templates

---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+Starts a hello world webserver.
+'''
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI()
+templates = Jinja2Templates(directory='templates')
+
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name='index.html',
+    )
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='127.0.0.1', port=8080, reload=True)
